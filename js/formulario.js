27b88afc9209443291f9850d00bb48f1a5f78678
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-10-02 19:51:01 +0200

    agregando img

--- js/formulario.js
+++ js/formulario.js
@@ -1,54 +1,54 @@
-function validarFormulario() {
+//function validarFormulario() {
     // Obtener los valores de los campos
-    var nombre = document.getElementById("nombre").value;
-    var email = document.getElementById("email").value;
-    var telefono = document.getElementById("telefono").value;
-    var asunto = document.getElementById("asunto").value;
-    var mensaje = document.getElementById("mensaje").value;
+ //   var nombre = document.getElementById("nombre").value;
+  //  var email = document.getElementById("email").value;
+  //  var telefono = document.getElementById("telefono").value;
+  //  var asunto = document.getElementById("asunto").value;
+  //  var mensaje = document.getElementById("mensaje").value;
 
     // Verificar si el mensaje tiene al menos 10 palabras
-    var palabras = mensaje.split(" ");
-    if (palabras.length < 10) {
+ //   var palabras = mensaje.split(" ");
+ //   if (palabras.length < 10) {
         // Mostrar el mensaje de error para el campo de mensaje
-        document.getElementById("mensaje-error").innerHTML = "El mensaje debe tener al menos 10 palabras.";
+     //   document.getElementById("mensaje-error").innerHTML = "El mensaje debe tener al menos 10 palabras.";
 
         // Devolver false para detener el envío del formulario
-        return false;
-    }
+     //   return false;
+   // }
 
     // Si todas las validaciones pasan, se puede enviar el formulario
-    return true;
-}
+   // return true;
+//}
 
 // Limpiar el mensaje de error para el campo de mensaje cuando se cambia el valor del campo
-document.getElementById("mensaje").addEventListener("input", function() {
-    document.getElementById("mensaje-error").innerHTML = "";
-});
+//document.getElementById("mensaje").addEventListener("input", function() {
+  //  document.getElementById("mensaje-error").innerHTML = "";
+//});
 
 
     // Obtener el modal y el botón para abrirlo
-    var modal = document.getElementById("myModal");
-    var btn = document.querySelector(".button");
+  //  var modal = document.getElementById("myModal");
+   // var btn = document.querySelector(".button");
 
     // Obtener el elemento para cerrar el modal
-    var closeModal = document.getElementById("closeModal");
+  //  var closeModal = document.getElementById("closeModal");
 
     // Cuando el usuario haga clic en el botón "Enviar", mostrar el modal
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
+   // btn.onclick = function() {
+    //    modal.style.display = "block";
+    //}
 
     // Cuando el usuario haga clic en la "x" del modal, ocultar el modal
-    closeModal.onclick = function() {
-        modal.style.display = "none";
-    }
+   // closeModal.onclick = function() {
+    //    modal.style.display = "none";
+   // }
 
     // Cuando el usuario haga clic en cualquier lugar fuera del modal, ocultar el modal
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
+   // window.onclick = function(event) {
+   //     if (event.target == modal) {
+          //  modal.style.display = "none";
+       // }
+    //}
 
 
 

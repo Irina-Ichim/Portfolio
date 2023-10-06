033b1c3af83cbc54d39f2ab21fedcdb327aab35b
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-10-06 23:37:46 +0200

    modificando los archivos del formulario

--- js/formulario.js
+++ js/formulario.js
@@ -1,66 +1,66 @@
-let prev = document.querySelector(".prev");
-let next = document.querySelector(".next");
-let box = document.querySelector(".box");
+function validarAsunto() {
+  let inputAsunto = document.getElementById('asunto');
+  let mensajeError = document.getElementById('mensaje-asunto-error');
+  
+  let palabras = inputAsunto.value.trim().split(/\s+/);
+  let cantidadPalabras = palabras.length;
 
-let degrees = 0;
+  if (cantidadPalabras > 30) {
+      mensajeError.textContent = 'El asunto no debe exceder las 30 palabras.';
+  } else {
+      mensajeError.textContent = '';
+  }
+}
 
-prev.addEventListener("click", function () {
-  degrees -= 45;
-  box.style.transform = `perspective(1000px) rotateY(${degrees}deg)`;
-});
+function validarTelefono() {
+  let inputTelefono = document.getElementById('telefono');
+  let mensajeError = document.getElementById('mensaje-telefono-error'); 
 
-next.addEventListener("click", function () {
-  degrees += 45;
-  box.style.transform = `perspective(1000px) rotateY(${degrees}deg)`;
-});
+  if (!/^[0-9]{9,}$/.test(inputTelefono.value.trim())) {
+      mensajeError.textContent = 'El teléfono debe tener al menos 9 números.';
+  } else {
+      mensajeError.textContent = '';
+  }
+}
 
-//function validarFormulario() {
-// Obtener los valores de los campos
-//   var nombre = document.getElementById("nombre").value;
-//  var email = document.getElementById("email").value;
-//  var telefono = document.getElementById("telefono").value;
-//  var asunto = document.getElementById("asunto").value;
-//  var mensaje = document.getElementById("mensaje").value;
+function enviarFormulario() {
+  let nombre = document.getElementById('nombre').value.trim();
+  let email = document.getElementById('email').value.trim();
+  let telefono = document.getElementById('telefono').value.trim();
+  let asunto = document.getElementById('asunto').value.trim();
 
-// Verificar si el mensaje tiene al menos 10 palabras
-//   var palabras = mensaje.split(" ");
-//   if (palabras.length < 10) {
-// Mostrar el mensaje de error para el campo de mensaje
-//   document.getElementById("mensaje-error").innerHTML = "El mensaje debe tener al menos 10 palabras.";
+  if (nombre === '' || email === '' || telefono === '' || asunto === '') {
+      mostrarAlerta('Por favor, completa todos los campos requeridos.');
+      return;
+  }
 
-// Devolver false para detener el envío del formulario
-//   return false;
-// }
+  let modal = document.getElementById('myModal');
+  modal.style.display = 'block';
 
-// Si todas las validaciones pasan, se puede enviar el formulario
-// return true;
-//}
+  document.getElementById('mensaje-confirmacion').style.display = 'block';
+  document.getElementById('texto-confirmacion').style.display = 'block';
 
-// Limpiar el mensaje de error para el campo de mensaje cuando se cambia el valor del campo
-//document.getElementById("mensaje").addEventListener("input", function() {
-//  document.getElementById("mensaje-error").innerHTML = "";
-//});
+  cerrarAlerta();
+}
 
-// Obtener el modal y el botón para abrirlo
-//  var modal = document.getElementById("myModal");
-// var btn = document.querySelector(".button");
+function mostrarAlerta(mensaje) {
+  let alerta = document.getElementById('custom-alert');
+  let mensajeAlerta = document.getElementById('alert-message');
 
-// Obtener el elemento para cerrar el modal
-//  var closeModal = document.getElementById("closeModal");
+  mensajeAlerta.textContent = mensaje;
+  alerta.style.display = 'block';
+}
 
-// Cuando el usuario haga clic en el botón "Enviar", mostrar el modal
-// btn.onclick = function() {
-//    modal.style.display = "block";
-//}
+function cerrarAlerta() {
+  let alerta = document.getElementById('custom-alert');
+  alerta.style.display = 'none';
+}
 
-// Cuando el usuario haga clic en la "x" del modal, ocultar el modal
-// closeModal.onclick = function() {
-//    modal.style.display = "none";
-// }
+function cerrarModal() {
+  let modal = document.getElementById('myModal');
+  let alerta = document.getElementById('custom-alert');
 
-// Cuando el usuario haga clic en cualquier lugar fuera del modal, ocultar el modal
-// window.onclick = function(event) {
-//     if (event.target == modal) {
-//  modal.style.display = "none";
-// }
-//}
+  modal.style.display = 'none';
+  alerta.style.display = 'none';
+}
+

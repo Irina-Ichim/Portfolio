e28867caf94063a63319a8016b5ce347af24d01c
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-10-06 23:56:10 +0200

    dar formato a los documentos

--- js/galeria.js
+++ js/galeria.js
@@ -1,18 +1,7 @@
 const carousel = document.querySelector(".gallery");
-const prevBtn = document.getElementById("prevBtn");
-const nextBtn = document.getElementById("nextBtn");
+
 const boxes = document.querySelectorAll(".box");
 let currentIndex = 0;
-
-prevBtn.addEventListener("click", () => {
-  currentIndex = (currentIndex - 1 + boxes.length) % boxes.length;
-  updateCarousel();
-});
-
-nextBtn.addEventListener("click", () => {
-  currentIndex = (currentIndex + 1) % boxes.length;
-  updateCarousel();
-});
 
 function updateCarousel() {
   const angle = (360 / boxes.length) * currentIndex;
